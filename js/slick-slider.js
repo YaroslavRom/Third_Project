682ea5fe6e6af60fb9a21dd0feebb66fd9421b9c
YaroslavRom <yaroslavromaniuk@gmail.com>
2021-07-05 17:35:44 +0300

    ALL DONE and WORKING

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -13,12 +13,34 @@ $(document).ready(function () {
     });
 });
 
-$('.responsive-link').onclick(function (e){
-    e.preventDefault();
-    $('.forms-background').fadeIn(600);
+// $('.responsive-link').onclick(function (e){
+//     e.preventDefault();
+//     $('.forms-background').fadeIn(600);
+// });
+//
+// $('.closing-x').onclick(function () {
+//     $('.forms-background').fadeOut(600);
+//     $('.form-its-self').fadeOut(600);
+//
+// })
+
+
+
+$(document).ready(function (){
+    $('.responsive-link').click(function (){
+        $('.form-its-self').fadeIn(1000);
+        $('.forms-background').fadeIn(1001);
+    });
 });
 
-$('.closing-x').click(function () {
-    $('.forms-background').fadeOut(600);
+$(document).ready(function (){
+    $('.closing-x').click(function (){
+        $('.close-form').click(function (){
 
-})
+
+        $(".form-its-self").fadeOut(1000);
+        $('.forms-background').fadeOut(1000);
+
+        });
+    });
+});
