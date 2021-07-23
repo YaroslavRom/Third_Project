7340cb127b00cd7195438802c7c0e72a67148d7e
Yaroslav <86099208+YaroslavRom@users.noreply.github.com>
2021-07-23 14:58:10 +0300

    Update slick-slider.js

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -13,16 +13,6 @@ $(document).ready(function () {
     });
 });
 
-// $('.responsive-link').onclick(function (e){
-//     e.preventDefault();
-//     $('.forms-background').fadeIn(600);
-// });
-//
-// $('.closing-x').onclick(function () {
-//     $('.forms-background').fadeOut(600);
-//     $('.form-its-self').fadeOut(600);
-//
-// })
 
 
 
